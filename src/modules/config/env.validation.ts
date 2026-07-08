1346578d6650a27510c5e05ef043bf5b35514845
원민관 <a4ueo@naver.com>
2026-07-08 21:03:33 +0900

    feat: env validation schema 추가

---
+++ src/modules/config/env.validation.ts
@@ -0,0 +1,11 @@
+import Joi from 'joi';
+
+export const envValidationSchema = Joi.object({
+  PORT: Joi.number().port().default(3000),
+  ALLOWED_ORIGINS: Joi.string().optional().allow(''),
+  DB_HOST: Joi.string().default('127.0.0.1'),
+  DB_PORT: Joi.number().port().default(3306),
+  DB_USERNAME: Joi.string().default('root'),
+  DB_PASSWORD: Joi.string().allow('').default(''),
+  DB_DATABASE: Joi.string().default('BLOG'),
+});
