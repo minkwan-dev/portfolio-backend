8027e7e52be1408201eb91d6a730a2e7ce5923dc
원민관 <a4ueo@naver.com>
2026-07-11 19:42:48 +0900

    refactor: blog-api에서 UserModule 제거

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppConfigModule } from '@/modules/config/config.module';
 import { DatabaseModule } from '@/modules/database/database.module';
-import { UserModule } from '@/modules/user/user.module';
 import { BlogModule } from './modules/blog/blog.module';
 
 @Module({
   imports: [
     AppConfigModule,
     DatabaseModule,
-    UserModule,
     BlogModule
   ],
 })
