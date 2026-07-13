a30a4ed216b86b25c3f582d299bb18bfb242595e
원민관 <a4ueo@naver.com>
2026-07-13 21:06:47 +0900

    refactor: AppModule imports를 변수로 분리하고 spread 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,11 @@ import { AppConfigModule } from '@/modules/config/config.module';
 import { DatabaseModule } from '@/modules/database/database.module';
 import { BlogModule } from './modules/blog/blog.module';
 
+const infrastructureMoudules = [AppConfigModule, DatabaseModule];
+const featureModules = [BlogModule];
+
 @Module({
-  imports: [
-    AppConfigModule,
-    DatabaseModule,
-    BlogModule
-  ],
+  imports: [...infrastructureMoudules, ...featureModules],
 })
 
 export class AppModule {}
