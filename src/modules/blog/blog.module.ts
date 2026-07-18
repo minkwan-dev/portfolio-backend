d0e0aaec45a8f81c6b0c889b0a2a36d29e6556ca
원민관 <a4ueo@naver.com>
2026-07-18 13:14:46 +0900

    feat: BlogModule에 PostController 등록

--- src/modules/blog/blog.module.ts
+++ src/modules/blog/blog.module.ts
@@ -6,11 +6,13 @@ import { PostTag } from '@/modules/blog/entities/post-tag.entity';
 import { Series } from '@/modules/blog/entities/series.entity';
 import { Tag } from '@/modules/blog/entities/tag.entity';
 import { PostService } from '@/modules/blog/post/post.service';
+import { PostController } from '@/modules/blog/post/post.controller';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Series, Post, Tag, PostTag, Comment]),
   ],
+  controllers: [PostController],
   providers: [PostService],
   exports: [PostService],
 })
