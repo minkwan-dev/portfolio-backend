ee3510ac2575adcfea389f438103103045c53783
원민관 <a4ueo@naver.com>
2026-07-22 21:29:16 +0900

    refactor: BlogModule PostModule aggregator로 변경

--- src/modules/blog/blog.module.ts
+++ src/modules/blog/blog.module.ts
@@ -1,20 +1,9 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Comment } from '@/modules/blog/entities/comment.entity';
-import { Post } from '@/modules/blog/entities/post.entity';
-import { PostTag } from '@/modules/blog/entities/post-tag.entity';
-import { Series } from '@/modules/blog/entities/series.entity';
-import { Tag } from '@/modules/blog/entities/tag.entity';
-import { PostService } from '@/modules/blog/post/post.service';
-import { PostController } from '@/modules/blog/post/post.controller';
+import { PostModule } from '@/modules/blog/post/post.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([Series, Post, Tag, PostTag, Comment]),
-  ],
-  controllers: [PostController],
-  providers: [PostService],
-  exports: [PostService],
+  imports: [PostModule],
+  exports: [PostModule],
 })
 
 export class BlogModule {}
