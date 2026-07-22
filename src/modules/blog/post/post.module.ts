81bd2fb917eeea47efc7913fc27d77a021d05ae4
원민관 <a4ueo@naver.com>
2026-07-22 21:28:08 +0900

    feat: PostModule 분리

---
+++ src/modules/blog/post/post.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Post } from '@/modules/blog/entities/post.entity';
+import { PostTag } from '@/modules/blog/entities/post-tag.entity';
+import { Tag } from '@/modules/blog/entities/tag.entity';
+import { PostController } from './post.controller';
+import { PostService } from '@/modules/blog/post/post.service';
+import { PostRepository } from '@/modules/blog/post/post.repository';
+import { PostTagRepository } from '@/modules/blog/post/post-tag.repository';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Post, PostTag, Tag])],
+  controllers: [PostController],
+  providers: [PostRepository, PostTagRepository, PostService],
+  exports: [PostRepository, PostService],
+})
+
+export class PostModule {}
