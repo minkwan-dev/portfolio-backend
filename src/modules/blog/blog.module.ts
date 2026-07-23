fcf8febca9142e3087a7d5eeadd853ffc0a5ce42
원민관 <a4ueo@naver.com>
2026-07-23 21:19:09 +0900

    feat: BlogModule CommentModule 등록

--- src/modules/blog/blog.module.ts
+++ src/modules/blog/blog.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PostModule } from '@/modules/blog/post/post.module';
+import { CommentModule } from '@/modules/blog/comment/comment.module';
 
 @Module({
-  imports: [PostModule],
+  imports: [PostModule, CommentModule],
   exports: [PostModule],
 })
 
