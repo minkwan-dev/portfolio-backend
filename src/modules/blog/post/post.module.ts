3e8320630c88896354e8d66536e6cc7b7294108f
원민관 <a4ueo@naver.com>
2026-07-23 21:22:12 +0900

    fix: PostModule에 Series 엔티티 등록

--- src/modules/blog/post/post.module.ts
+++ src/modules/blog/post/post.module.ts
@@ -3,13 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Post } from '@/modules/blog/entities/post.entity';
 import { PostTag } from '@/modules/blog/entities/post-tag.entity';
 import { Tag } from '@/modules/blog/entities/tag.entity';
+import { Series } from '@/modules/blog/entities/series.entity';
 import { PostController } from './post.controller';
 import { PostService } from '@/modules/blog/post/post.service';
 import { PostRepository } from '@/modules/blog/post/post.repository';
 import { PostTagRepository } from '@/modules/blog/post/post-tag.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Post, PostTag, Tag])],
+  imports: [TypeOrmModule.forFeature([Post, PostTag, Tag, Series])],
   controllers: [PostController],
   providers: [PostRepository, PostTagRepository, PostService],
   exports: [PostRepository, PostService],
